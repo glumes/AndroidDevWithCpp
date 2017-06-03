803fc81570ce539e1c9ec34518bc5ce3a2e0c2a3
glumes <zhaoying9402@gmail.com>
2017-06-04 00:30:59 +0800

    add c practice

---
+++ app/src/main/cpp/include/c_lib/c_file.c
@@ -0,0 +1,15 @@
+//
+// Created by glumes on 2017/6/4.
+//
+
+/**
+ * C file Content
+ */
+#include <jni.h>
+#include <android/log.h>
+
+#define TAG "CONFIGTEST"
+#define LOGE(format, ...)  __android_log_print(ANDROID_LOG_ERROR, TAG, format, ##__VA_ARGS__)
+#define LOGD(format, ...)  __android_log_print(ANDROID_LOG_DEBUG, TAG, format, ##__VA_ARGS__)
+
+
