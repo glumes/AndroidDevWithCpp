af5fac60e76709e5cef8c6da7c89f54855b6ef20
glumes <zhaoying9402@gmail.com>
2018-04-20 00:10:20 +0800

    refactor

--- app/src/main/java/com/glumes/androidcppsolib/ui/JNIMethodActivity.kt
+++ app/src/main/java/com/glumes/androidcppsolib/ui/JNIMethodActivity.kt
@@ -34,11 +34,12 @@ class JNIMethodActivity : BaseListActivity() {
     }
 
     private fun initData() {
-        items.add(SampleModel("getNativeString", NATIVE_STRING))
-        items.add(SampleModel("getNativeIntArray", NATIVE_INT_ARRAY))
-        items.add(SampleModel("callStaticMethod", NATIVE_CALL_STATIC_METHOD))
-        items.add(SampleModel("callInstanceMethod", NATIVE_CALL_INSTANCE_METHOD))
-        items.add(SampleModel("callSuperMethod", NATIVE_CALL_SUPER_METHOD))
+        items.add(SampleModel("基本数据类型操作",))
+        items.add(SampleModel("native 返回 string 类型", NATIVE_STRING))
+        items.add(SampleModel("native 返回 array 类型", NATIVE_INT_ARRAY))
+        items.add(SampleModel("native 调用 类 static 方法", NATIVE_CALL_STATIC_METHOD))
+        items.add(SampleModel("native 调用 对象实例方法", NATIVE_CALL_INSTANCE_METHOD))
+        items.add(SampleModel("native 调用父类方法", NATIVE_CALL_SUPER_METHOD))
         items.add(SampleModel("Cache", NATIVE_ACCESS_CACHE))
     }
 }
